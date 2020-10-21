df084aad0fca7a4e8c4e6ee9a7022747f3fd0780
12Wass <wassimdah@gmail.com>
2020-10-21 17:48:59 +0200

    Adding vig() function

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -34,9 +34,38 @@ function leet(string){
 
 function verlan(string){
     if (typeof string !== "string" || string === "") return "";
-    
+
     return chaine
     .split(" ")
     .map((word) => word.split("").reverse().join(""))
     .join(" ");
 }
+
+function yoda(string){
+    if (typeof string !== "string" || string === "") return "";
+    return chaine.split(" ").reverse().join(" "); 
+}
+
+function vig(string, code){
+    if (typeof string !== "string") return ""; 
+    if (string.length === 0) return string;
+
+    while (code.length < string.length){
+        code += code;
+    };
+    code = code.substr(0, string.length); 
+    let codeIndex = 0; 
+
+    return string.split("").map((letter, index) => {
+        letter = letter.toLowerCase(); 
+        const aCode = "a".charCodeAt(0);
+        const letterNumber = letter.charCodeAt(0) - aCode; // [0 - 25]
+        
+        if (letterNumber < 0 || letterNumber > 25) return letter;
+        const codeNumber = code.charCodeAt(codeIndex) - aCode;
+        codeIndex++; 
+
+        return String.fromCharCode(((letterNimber + codeNumber) % 26) + aCode);
+    }).join(""); 
+}
+
