7d70d402092da762200f31954cf6d110db7f1fcc
12Wass <wassimdah@gmail.com>
2020-10-23 18:27:14 +0200

    Improving type_check_v1

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,10 +1,24 @@
 function type_check_v1(arg, type){
-    if (typeof(arg) !== type) {
-        return false;
+    if (typeof(arg) === "object"){
+        if (Array.isArray(arg)){
+            if (type === "array"){
+                return true;
+            }
+            return false; 
+        }
     }
-    return true; 
+        if (typeof(arg) !== type) {
+            return false;
+        }
+        return true; 
+ }
+
+function type_check_v2(arg, conf){
+    if (typeof(arg) == conf.type && arg == conf.value){
+        return true;
+    }
+    return false; 
 }
 
-function type_check_v2(){
-    
-}
+console.log(type_check_v1({"1": "1", "2": "2", "3": "3"}, "object")); // JSON 
+console.log(type_check_v1(["1", "2", "3"], "array")); // Array
